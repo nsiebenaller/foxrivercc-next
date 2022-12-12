09e8c6abba03f797bdf9c7d84033623e5f0a690c
Nicholas <nicholaspaulsiebenaller@gmail.com>
2022-12-11 19:24:34 -0600

    chore: add base domain to hello api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,11 +3,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   name: string;
+  baseDomain: string;
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const baseDomain = String(process.env.BASE_DOMAIN);
+  res.status(200).json({ name: "John Doe", baseDomain });
 }
