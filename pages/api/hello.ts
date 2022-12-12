278df5a5ee342ab7e68808250ddadca71af0525a
Nicholas <nicholaspaulsiebenaller@gmail.com>
2022-12-11 20:51:58 -0600

    chore: test db connection

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,15 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { Client, Pool } from "pg";
 
 type Data = {
   name: string;
   baseDomain: string;
+  rows: any;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+  const rows = await new Promise((resolve) => {
+    pool.query("SELECT NOW()", (_err, res) => {
+      resolve(res.rows);
+      pool.end();
+    });
+  });
+
+  console.log(rows);
+
   const baseDomain = String(process.env.BASE_DOMAIN);
-  res.status(200).json({ name: "John Doe", baseDomain });
+  res.status(200).json({ name: "John Doe", baseDomain, rows });
 }
